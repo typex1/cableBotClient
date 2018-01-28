95413c3e5069ddcb1c861c74fb5324a20cd6f352
Typex <typex@web.de>
2018-01-28 21:22:53 +0100

    update

--- python/cableBotTest_Xdirection.py
+++ python/cableBotTest_Xdirection.py
@@ -21,9 +21,9 @@ headers = {'Content-Type': 'application/json'}
 #response = requests.post(base_url+'linesURL/1a/3/3/0/17/7/0/185', headers=headers)
 #URL structure: /linesURL/{id}/{x1}/{y1}/{z1}/{x2}/{y2}/{z2}/{servo}
 #move in x direction:
-#response = requests.post(base_url+'linesURL/1a/3/0/0/'+str(length)+'/0/0/185', headers=headers)
+response = requests.post(base_url+'linesURL/1a/3/0/0/'+str(length)+'/0/0/185', headers=headers)
 #move in y direction:
-response = requests.post(base_url+'linesURL/1a/0/3/0/0/'+str(length)+'/0/185', headers=headers)
+#response = requests.post(base_url+'linesURL/1a/0/3/0/0/'+str(length)+'/0/185', headers=headers)
 
 #dump line data:	
 response = requests.get(base_url+'lines')
